952539d50f9e6f6e27e56c49a94f4fec7ddd0a32
Konsta <konsta09@gmail.com>
2015-09-30 20:22:37 +0300

    sdcard: don't symlink playlists
    * Android doesn't create Playlist directory so it usually doesn't
      exist.

--- sparse/usr/bin/droid/android-links.sh
+++ sparse/usr/bin/droid/android-links.sh
@@ -8,7 +8,6 @@ if [ ! -h "/home/nemo/Pictures/AndroidDCIM" ]; then
     ln -s $MNT/Movies /home/nemo/Videos/AndroidMovies
     ln -s $MNT/Music /home/nemo/Music/AndroidMusic
     ln -s $MNT/Pictures /home/nemo/Pictures/AndroidPictures
-    ln -s $MNT/Playlists /home/nemo/Playlists/AndroidPlaylists
     ln -s $MNT/Podcasts /home/nemo/Music/AndroidPodcasts
 fi
 
